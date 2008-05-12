7f95b568b3b362e272eda0ada40194baed62ff42
drulli <drulli@dev.java.net>
2008-05-12 16:42:25 +0000

    Added newInstance method for descriptors.

--- src/main/java/hudson/plugins/checkstyle/CheckStyleReporterDescriptor.java
+++ src/main/java/hudson/plugins/checkstyle/CheckStyleReporterDescriptor.java
@@ -1,7 +1,11 @@
 package hudson.plugins.checkstyle;
 
+import hudson.maven.MavenReporter;
 import hudson.plugins.checkstyle.util.PluginDescriptor;
 import hudson.plugins.checkstyle.util.ReporterDescriptor;
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.StaplerRequest;
 
 /**
  * Descriptor for the class {@link CheckStyleReporter}. Used as a singleton. The
@@ -18,6 +22,12 @@ public class CheckStyleReporterDescriptor extends ReporterDescriptor {
      */
     public CheckStyleReporterDescriptor(final PluginDescriptor pluginDescriptor) {
         super(CheckStyleReporter.class, pluginDescriptor);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public MavenReporter newInstance(final StaplerRequest request, final JSONObject formData) throws FormException {
+        return request.bindParameters(CheckStyleReporter.class, getPublisherDescriptor().getPluginName() + ".");
     }
 }
 
