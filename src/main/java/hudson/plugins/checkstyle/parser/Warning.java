750874e6a0abc83e35ccd03cac8a3110d7fc55db
drulli <drulli@dev.java.net>
2008-06-28 12:46:22 +0000

    Fixed jelly scripts so that the new tabview could be used with tasks plug-in, too.

--- src/main/java/hudson/plugins/checkstyle/parser/Warning.java
+++ src/main/java/hudson/plugins/checkstyle/parser/Warning.java
@@ -100,15 +100,6 @@ public class Warning extends AbstractAnnotation {
         this.tooltip = tooltip;
     }
 
-    /**
-     * Gets the associated file name of this bug (without path).
-     *
-     * @return the short file name
-     */
-    public String getShortFileName() {
-        return StringUtils.substringAfterLast(getFileName(), "/");
-    }
-
     /** {@inheritDoc} */
     public String getToolTip() {
         return tooltip;
