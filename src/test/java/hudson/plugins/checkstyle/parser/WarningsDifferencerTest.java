48fddd0e316fd263abc49c36bf3eb2a54c56d144
drulli <drulli@dev.java.net>
2008-08-30 16:03:20 +0000

    [FIXED HUDSON-1727] Fixed short filename computation if there is no path segment in the filename.

--- src/test/java/hudson/plugins/checkstyle/parser/WarningsDifferencerTest.java
+++ src/test/java/hudson/plugins/checkstyle/parser/WarningsDifferencerTest.java
@@ -11,9 +11,11 @@ import hudson.plugins.checkstyle.util.model.Priority;
 public class WarningsDifferencerTest extends AnnotationDifferencerTest {
     /** {@inheritDoc} */
     @Override
-    public FileAnnotation createAnnotation(final Priority priority, final String message, final String category,
+    public FileAnnotation createAnnotation(final String fileName, final Priority priority, final String message, final String category,
             final String type, final int start, final int end) {
-        return new Warning(priority, message, message, message, start, end);
+        Warning warning = new Warning(priority, message, message, message, start, end);
+        warning.setFileName(fileName);
+        return warning;
     }
 }
 
