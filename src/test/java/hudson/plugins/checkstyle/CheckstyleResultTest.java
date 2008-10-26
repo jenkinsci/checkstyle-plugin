5d98bc0405a139ab19136eecabe726ca9ef903b5
drulli <drulli@dev.java.net>
2008-10-26 17:15:30 +0000

    Added visualization of zero-warnings records.

--- src/test/java/hudson/plugins/checkstyle/CheckstyleResultTest.java
+++ src/test/java/hudson/plugins/checkstyle/CheckstyleResultTest.java
@@ -1,7 +1,9 @@
 package hudson.plugins.checkstyle;
 
+import static junit.framework.Assert.*;
 import hudson.model.AbstractBuild;
 import hudson.plugins.checkstyle.util.AbstractAnnotationsBuildResultTest;
+import hudson.plugins.checkstyle.util.AnnotationsBuildResult;
 import hudson.plugins.checkstyle.util.ParserResult;
 
 /**
@@ -18,6 +20,32 @@ public class CheckstyleResultTest extends AbstractAnnotationsBuildResultTest<Che
     @Override
     protected CheckStyleResult createBuildResult(final AbstractBuild<?, ?> build, final ParserResult project, final CheckStyleResult previous) {
         return new CheckStyleResult(build, project, previous);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void verifyHighScoreMessage(final int expectedZeroWarningsBuildNumber, final boolean expectedIsNewHighScore, final long expectedHighScore, final int gap, final CheckStyleResult result) {
+        if (result.hasNoAnnotations() && result.getDelta() == 0) {
+            assertTrue(result.getDetails().contains(Messages.Checkstyle_ResultAction_NoWarningsSince(expectedZeroWarningsBuildNumber)));
+            if (expectedIsNewHighScore) {
+                long days = AnnotationsBuildResult.getDays(expectedHighScore);
+                if (days == 1) {
+                    assertTrue(result.getDetails().contains(Messages.Checkstyle_ResultAction_OneHighScore()));
+                }
+                else {
+                    assertTrue(result.getDetails().contains(Messages.Checkstyle_ResultAction_MultipleHighScore(days)));
+                }
+            }
+            else {
+                long days = AnnotationsBuildResult.getDays(gap);
+                if (days == 1) {
+                    assertTrue(result.getDetails().contains(Messages.Checkstyle_ResultAction_OneNoHighScore()));
+                }
+                else {
+                    assertTrue(result.getDetails().contains(Messages.Checkstyle_ResultAction_MultipleNoHighScore(days)));
+                }
+            }
+        }
     }
 }
 
