109877ede579363db1d9c4fcb8cca10172ef02f9
drulli <drulli@dev.java.net>
2010-03-26 08:04:15 +0000

    [HUDSON-5225] Updated unit test to new Checkstyle messages.

--- src/test/java/hudson/plugins/checkstyle/rules/CheckStyleRulesTest.java
+++ src/test/java/hudson/plugins/checkstyle/rules/CheckStyleRulesTest.java
@@ -19,8 +19,9 @@ public class CheckStyleRulesTest {
 
         assertEquals("Wrong number of rules detected.", 114, reader.getRules().size());
         assertNotNull("No EmptyBlock rule found.", reader.getRule("EmptyBlock"));
-        assertNotSame("No description for EmptyBlock found.", Rule.UNDEFINED_DESCRIPTION, reader.getRule("EmptyBlock").getDescription());
-        assertNotSame("No description for EmptyBlock found.", Rule.UNDEFINED_DESCRIPTION, reader.getDescription("EmptyBlock"));
+        assertSame("Description for EmptyBlock found.", Rule.UNDEFINED_DESCRIPTION, reader.getRule("EmptyBlock").getDescription());
+        assertNotSame("No description for AnnotationUseStyle found.", Rule.UNDEFINED_DESCRIPTION, reader.getRule("AnnotationUseStyle").getDescription());
+        assertNotSame("No description for AnnotationUseStyle found.", Rule.UNDEFINED_DESCRIPTION, reader.getDescription("AnnotationUseStyle"));
         assertSame("No default text available for undefined rule.", Rule.UNDEFINED_DESCRIPTION, reader.getRule("Undefined").getDescription());
 
         for (Rule rule : reader.getRules()) {
