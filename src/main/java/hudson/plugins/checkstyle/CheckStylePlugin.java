7c3d7feb48b885c48db99606b5443c45b63822f3
Ulli Hafner <ullrich.hafner@gmail.com>
2014-01-30 21:59:09 +0100

    [ FIXED JENKINS-20070] Clear SAX system property afterwards.

--- src/main/java/hudson/plugins/checkstyle/CheckStylePlugin.java
+++ src/main/java/hudson/plugins/checkstyle/CheckStylePlugin.java
@@ -1,6 +1,6 @@
 package hudson.plugins.checkstyle;
 
-import org.apache.xerces.parsers.SAXParser;
+import hudson.plugins.analysis.util.SaxSetup;
 
 import hudson.Plugin;
 
@@ -12,18 +12,15 @@ import hudson.plugins.checkstyle.rules.CheckStyleRules;
  * @author Ulli Hafner
  */
 public class CheckStylePlugin extends Plugin {
-    /** Property of SAX parser factory. */
-    static final String SAX_DRIVER_PROPERTY = "org.xml.sax.driver";
-
     @Override
     public void start() {
-        String oldProperty = System.getProperty(SAX_DRIVER_PROPERTY);
-        System.setProperty(SAX_DRIVER_PROPERTY, SAXParser.class.getName());
+        SaxSetup sax = new SaxSetup();
 
-        CheckStyleRules.getInstance().initialize();
-
-        if (oldProperty != null) {
-            System.setProperty(SAX_DRIVER_PROPERTY, oldProperty);
+        try {
+            CheckStyleRules.getInstance().initialize();
+        }
+        finally {
+            sax.cleanup();
         }
     }
 }
